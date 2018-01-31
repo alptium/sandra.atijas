abdf35e92520064a1bb015c0777e667491c9fc44
saki3663 <Sandra.Atijas@gmail.com>
2018-01-31 17:29:27 +0100

    Calculator

---
+++ Caluclator/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		
+		System.out.println("Enter your first number: ");
+		
+		a = sc.nextInt();
+		
+		System.out.println("Enter your second number: ");
+		
+		b = sc.nextInt();
+		
+		System.out.println("Write the operation you want (* / + -): ");
+		
+		String op = sc.next();
+		
+		if (op.equals("/") && b == 0) {
+			
+			System.out.println("You cannot divide with 0.");
+			System.out.println("Write again enter your second number: ");
+
+			 b = sc.nextInt();
+		}
+
+		switch (op) {
+        case "+":  c = a +b;
+                 break;
+        case "-":   c = a - b;
+        		break;
+        case "*":   c = a * b;
+				break;
+        case "/":   c = a / b;
+			    break;
+		} 
+		
+		System.out.println("First number is: " + a);
+		System.out.println("Second number is: " + b);
+		System.out.println("The result is: " + c );
+				
+	}
+
+}
