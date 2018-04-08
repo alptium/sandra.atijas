1d580b1904ab923a828064371ef401fb3ac95d2c
saki3663 <Sandra.Atijas@gmail.com>
2018-04-08 12:53:41 +0200

    #17 hibernate

--- restaurant-ordering/src/restaurant/Main.java
+++ restaurant-ordering/src/restaurant/Main.java
@@ -1,21 +1,31 @@
 package restaurant;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
 	
-	private static SessionFactory factory; 
+	private static SessionFactory sessionFactory;
 	
 	public static void main(String[] args) {
-		
+		Session session = null;
 		try {
-			factory = new Configuration().configure().buildSessionFactory();
-			}catch (Throwable ex) { 
+			// This step will read hibernate.cfg.xml and prepare hibernate for use
+			sessionFactory = new Configuration().configure().buildSessionFactory();
+			session =sessionFactory.openSession();
+			
+			} 
+		catch (Throwable ex) { 
 				System.err.println("Failed to create sessionFactory object." + ex);
 				throw new ExceptionInInitializerError(ex);
-				}
-		
+		}
+		finally{
+			session.flush();
+			session.close();
+							
+		}
+	
 		ManageOrder MO = new ManageOrder();
 		MO.lisOrders();
 
